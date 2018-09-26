6decbb399bf15d17d0f4a65a6145e4cd298e40a1
SRenganathan <renganathan.sekar@gmail.com>
2018-09-26 20:33:10 +0900

    Added a program for generating a random password

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,7 +1,20 @@
 import random
 import string
-special_characters = '!@#$%^&*~_'
-char_set = string.ascii_uppercase + string.digits + string.ascii_lowercase + special_characters
-print (''.join(random.sample(char_set*6, 10)))
 
+print("This program will generate a password with random characters")
+print("Enter the password difficulty:")
 
+user_difficulty = input("Type H for Hard, M for Medium and W for a weak password: ")
+number_of_digits = int(input("Enter the length of the password:"))
+
+if (user_difficulty == 'H'):
+    special_characters = '!@#$%^&*~_'
+    char_set = string.ascii_uppercase + string.digits + string.ascii_lowercase + special_characters
+    print (''.join(random.sample(char_set*6, number_of_digits)))
+elif (user_difficulty == 'M'):
+    special_characters = '!@#$'
+    char_set = string.ascii_uppercase + string.digits + string.ascii_lowercase + special_characters
+    print (''.join(random.sample(char_set*6, number_of_digits)))
+else:
+    char_set = string.ascii_uppercase + string.digits + string.ascii_lowercase 
+    print (''.join(random.sample(char_set*6, number_of_digits)))
